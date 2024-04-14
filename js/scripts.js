17a50185a1f0c0962ec4f4fca8e8eaab652acce4
diegomtz <diego.martinez@kokonut.company>
2024-04-14 05:46:27 -0600

    Agrega archivos iniciales del proyecto

---
+++ js/scripts.js
@@ -0,0 +1,63 @@
+const genParams = {
+    primeraGen: true,
+    segundaGen: true,
+    terceraGen: true,
+    cuartaGen: true,
+    quintaGen: true,
+    sextaGen: true,
+    septimaGen: true,
+    octavaGen: true,
+    novenaGen: true
+};
+document.addEventListener("DOMContentLoaded", () => {
+    const generationIcons = document.querySelectorAll('.gen-icon');
+
+    // Event listener para cada ícono de generación
+    generationIcons.forEach(icon => {
+        icon.addEventListener('click', () => {
+            console.log('o');
+            icon.classList.toggle('active'); // Alternar clase 'active' al hacer clic
+        });
+    });
+});
+
+// Función para generar un Pokémon aleatorio
+async function generateRandomPokemon() {
+    const activeGenerations = Array.from(document.querySelectorAll('.gen-icon.active'))
+        .map(icon => icon.getAttribute('data-gen'));
+
+    const randomGen = activeGenerations[Math.floor(Math.random() * activeGenerations.length)];
+    const randomPokemonNumber = Math.floor(Math.random() * 151) + 1;
+
+    const pokemonImage = document.getElementById('pokemon-image');
+    pokemonImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomPokemonNumber}.png`;
+
+    // Opcional: Mostrar la imagen durante 3 segundos
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    pokemonImage.src = 'assets/img/missgno.gif';
+}
+
+const getPokemonByGeneration = async (gen) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/generation/${gen}/`);
+    const data = await response.json();
+    return data.pokemon_species.map(pokemon => pokemon.name);
+};
+
+const getRandomPokemonList = async () => {
+    let allPokemon = [];
+
+    const activeGenerations = Array.from(document.querySelectorAll('.gen-icon.active'))
+        .map(icon => icon.getAttribute('data-gen'));
+
+    const randomGen = activeGenerations[Math.floor(Math.random() * activeGenerations.length)];
+    console.log(randomGen);
+
+    allPokemon = await getPokemonByGeneration(randomGen);
+    allPokemon.sort(function() {
+        return 0.5 - Math.random();
+    });
+      
+    const randomPokemon = allPokemon.slice(0, 20);
+    console.log(randomPokemon);
+    return randomPokemon;
+};
---
+++ js/sorteo.js
@@ -0,0 +1,106 @@
+let newPoke = 'Psyduck';
+let values = ['uno', 'dos', 'tres', 'cuatro', 'cinco', 'invisible'];
+
+function iniciarRuleta() {
+    const cantidadMinimaValores = 40;
+    const cantidadMaximaValores = 55;
+    const N = Math.floor(Math.random() * (cantidadMaximaValores - cantidadMinimaValores) + cantidadMinimaValores);
+    ejecutarConRetraso(generarLista(N));
+}
+
+function generarLista(N) {
+    let lista = [];
+    let incrementoMenor = 10;
+    let incrementoMayor = 200;
+    for (let i = 0; i < N; i++) {
+        if (i / N < 0.8) { // 80% de los números incrementando de 10 hasta cerca de 110
+            lista.push(Math.min(incrementoMenor, 115));
+            incrementoMenor += Math.floor(Math.random() * (4 - 2) + 2); // Incremento aleatorio entre 0 y 2
+        } else {
+            // Resto de los números incrementando más rápido hasta cerca de 400
+            lista.push(incrementoMayor);
+            incrementoMayor += Math.floor(Math.random() * (80 - 50) + 50 / (N - i)); // Incremento que se ajusta para alcanzar cerca de 400
+        }
+    }
+    return lista;
+}
+
+function ejecutarConRetraso(valoresRetraso) {
+    let index = 0;
+    let delayClass = false;
+
+    function llamarFuncionConRetraso() {
+        if (index < valoresRetraso.length) {
+            const retraso = valoresRetraso[index];
+
+            setTimeout(() => {
+                // Llamar a tu función aquí o realizar alguna acción
+                console.log(`Llamando a la función después de ${retraso} ms ${index}`);
+
+                // Incrementar el índice para pasar al siguiente valor de retraso
+                index++;
+
+                //Acciones a ejecutar
+                animacion();
+                if (retraso > 200 && !delayClass) {
+                    delayClass = true;
+                    crearEstilo();
+                }
+
+                // Llamar recursivamente para el siguiente valor de retraso
+                llamarFuncionConRetraso();
+            }, retraso);
+        } else {
+            finEjecucion();
+        }
+    }
+
+    // Comenzar el proceso llamando a la función con el primer retraso
+    llamarFuncionConRetraso();
+}
+
+function finEjecucion() {
+    const rouletteContainer = document.getElementById('roulette');
+    const divs = rouletteContainer.querySelectorAll('div');
+    eliminarEstilo();
+    setTimeout(() => {
+        divs[2].classList.add('animate__animated', 'animate__tada');
+    }, 500);
+    console.log(`Ganador: ${divs[2].innerText}`);
+    console.log('Todos los retardos han sido utilizados');
+}
+
+function crearEstilo() {
+    let styleSheet = document.createElement('style');
+    document.head.appendChild(styleSheet);
+
+    // Agregar una regla CSS a la hoja de estilos
+    styleSheet.sheet.insertRule('.roulette-container>div  { transition: font-size 0.2s ease; }', 0);
+}
+
+function eliminarEstilo() {
+    // Obtener la hoja de estilos que hemos creado
+    var styleSheet = document.querySelector('style');
+
+    if (styleSheet) {
+        // Remover la hoja de estilos del DOM
+        styleSheet.parentNode.removeChild(styleSheet);
+    } else {
+        console.warn('No se encontró ninguna hoja de estilos para eliminar.');
+    }
+}
+
+function animacion() {
+    const rouletteContainer = document.getElementById('roulette');
+    const divs = rouletteContainer.querySelectorAll('div');
+    const nuevoDiv = document.createElement('div');
+    nuevoDiv.textContent = newPoke;
+    nuevoDiv.className = 'invisible newItem';
+    rouletteContainer.insertBefore(nuevoDiv, divs[0]);
+    const divUpdated = rouletteContainer.querySelectorAll('div');
+    for (let i = 0; i < 5; i++) {
+        divUpdated[i].className = i == 0 ? values[i] + ' newItem' : values[i];
+    }
+    newPoke = divUpdated[divUpdated.length - 1].innerHTML;
+    divUpdated[divUpdated.length - 1].remove();
+}
